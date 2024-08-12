b0e20ca43d1b334b00eb2f3d0daa7b3f3257f667
ououn <33188248+ououn@users.noreply.github.com>
2024-08-12 00:52:13 -1000

    Add files via upload

---
+++ uv_point_copycat.js
@@ -0,0 +1,110 @@
+(function() {
+    var storedspls = [0,0,-1];
+    var button_c;
+    var button_p;
+    var button_q;
+
+    function getUVPointpls(){
+        let keypls = Mesh.selected[0].getSelectedFaces()[0];
+        let uvpls = Mesh.selected[0].getSelectedVertices()[0];
+        storedspls[0] = Mesh.selected[0]._static.properties.faces[keypls].uv[uvpls][0];
+        storedspls[1] = Mesh.selected[0]._static.properties.faces[keypls].uv[uvpls][1];
+        storedspls[2] = 0;
+        Canvas.updateUVs();
+        UVEditor.loadData();
+    }
+
+    function writeUVPointpls(){
+        let keypls = Mesh.selected[0].getSelectedFaces()[0];
+        let uvpls = Mesh.selected[0].getSelectedVertices()[0];
+        Mesh.selected[0]._static.properties.faces[keypls].uv[uvpls][0] = storedspls[0];
+        Mesh.selected[0]._static.properties.faces[keypls].uv[uvpls][1] = storedspls[1];
+        storedspls[2] = 1;
+        Canvas.updateUVs();
+        UVEditor.loadData();
+    }
+
+    function superUVPointpls(){
+        if ( !( storedspls[2] >= 0 && storedspls[2] <=1 ) ) {
+            getUVPointpls();
+        } else {
+            switch( storedspls[2] ) {
+                case 0:
+                    writeUVPointpls();
+                    break;
+                case 1:
+                    getUVPointpls();
+                    break;
+            }
+            //storedspls[2] = (storedspls[2] ? 0 : 1);
+        }
+    }
+
+    Plugin.register('uv_point_copycat', {
+        title: 'UV Point Copycat!',
+        icon: 'fas.fa-cat',
+        author: 'OuOun',
+        description: 'Easy copy & paste for selected mesh uv coordinate, made manual mapping purfect!',
+        about: 'Watch mme get da uv point for ya, it will bee quick, trust mme! Big thx to Gudf.',
+        tags: [],
+        version: '0.4.15',
+        variant: 'desktop',
+        onload() {
+            button_c = new Action('uv_point_copy', {
+                icon: 'fas.fa-cat',
+                color: 'x',
+                name: 'Summon UV Copycat',
+                category: 'uv',
+                description: 'Let mme keep dis UV cooconut for ya!',
+                condition: () => Mesh.selected.length == 1 && Mesh.selected[0].getSelectedVertices().length == 1 && Mesh.selected[0].getSelectedFaces().length == 1,
+                /* click: function() {
+                    Blockbench.showMessageBox({
+                        title: 'Helow Thre',
+                        icon: 'fas.fa-cat',
+                        message: '~',
+                        buttons: ['Hi']
+                    });
+                    getUVPointpls();
+                } */
+                click: function() {
+                    getUVPointpls();
+                }
+            });
+
+            button_p = new Action('uv_point_paste', {
+                icon: 'fas.fa-cat',
+                color: 'z',
+                name: 'Summon UV Pastecat',
+                category: 'uv',
+                description: 'Let mme place dis UV cooconut for ya!',
+                condition: () => Mesh.selected.length == 1 && Mesh.selected[0].getSelectedVertices().length == 1 && Mesh.selected[0].getSelectedFaces().length == 1,
+                click: function() {
+                    writeUVPointpls();
+                }
+            });
+
+            button_q = new Action('uv_point_quick', {
+                icon: 'fas.fa-cat',
+                color: '#FF8C00',
+                name: 'Summon UV Supercat',
+                category: 'uv',
+                description: 'Rotate between Copycat and Pastecat! Tho only a few can master dis skill!!',
+                condition: () => Mesh.selected.length == 1 && Mesh.selected[0].getSelectedVertices().length == 1 && Mesh.selected[0].getSelectedFaces().length == 1,
+                click: function() {
+                    superUVPointpls();
+                }
+            });
+
+            MenuBar.menus.uv.addAction(button_c);
+            MenuBar.menus.uv.addAction(button_p);
+            MenuBar.menus.uv.addAction(button_q);
+
+        },
+        onunload() {
+            button_c.delete();
+            button_p.delete();
+            button_q.delete();
+        }
+    });
+
+})()
